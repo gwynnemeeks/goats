5c2f58da43fbfa33ff1c8fdf50827d81230c1187
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-10 20:26:46 -0500

    printing goat cards

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Goat from '../Goat/Goat';
 
 class GoatCorral extends React.Component {
   render() {
     const { goats } = this.props;
 
+    const goatCards = goats.map((goat) => (
+    <Goat key={goat.id} goat={goat}/>
+    ));
+
     return (
         <div>
       <h2>Get Your Goat</h2>
-      { goats.map((goat) => (<h6>{goat.name}</h6>)) }
+      { goatCards }
       </div>
     );
   }
